a793d918ed2409f4e20db63de32bde5196b3064d
Ruan Gouveia <ruangouveia78@gmail.com>
2026-06-05 23:36:52 -0300

    chore: ocultar credenciais de banco de dados via variaveis de ambiente e ignorar arquivos locais de backup

--- src/main/java/com/badplay/repository/FilmeRepository.java
+++ src/main/java/com/badplay/repository/FilmeRepository.java
@@ -1,9 +1,15 @@
 package com.badplay.repository;
 
 import com.badplay.entity.Filme;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+    @EntityGraph(attributePaths = {"generos"})
+    List<Filme> findAll();
 }
--- src/main/java/com/badplay/repository/SerieRepository.java
+++ src/main/java/com/badplay/repository/SerieRepository.java
@@ -1,9 +1,15 @@
 package com.badplay.repository;
 
 import com.badplay.entity.Serie;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+    @EntityGraph(attributePaths = {"generos"})
+    List<Serie> findAll();
 }
